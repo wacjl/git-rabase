d4d1280a6bbc1c3f9c4fb2822be062eaa7f692e6
zhouhong <846297343@qq.com>
2021-09-23 14:16:39 +0800

    feat:cherrypick

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const fs = require('fs')
+const path = require('path')
+const { exec } = require('child_process');
+
+const flag = fs.existsSync(__dirname+'/src/my-vue-tools')
+const myDirname =  path.resolve(__dirname,'./src')
+if(!flag){
+  exec('git clone https://github.com/wacjl/my-vue-tools.git',{cwd:myDirname},function(error){
+    console.log(error)
+  })
+}else{
+  exec('git pull https://github.com/wacjl/my-vue-tools.git',{cwd:myDirname+'/my-vue-tools'},function(error){
+    console.log(error)
+  })
+}
+const test=2
